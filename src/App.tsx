f30804c2db6ae675fa96b8d898c55ec4824353b4
hsnam1211 <hsnam1211@naver.com>
2024-08-13 15:16:41 +0900

    svg 제거

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import NotFound from "@/pages/NotFound";
 import TestPage from "./pages/test/TestPage";
 import TheFlagger from "./pages/test/TheFlagger";
 import TheSpeeder from "./pages/test/TheSpeeder";
-import TheSvg from "./pages/test/TheSvg";
 
 // import "@hoosss-test/utils";
 
@@ -17,7 +16,6 @@ function App() {
         <Route path="*" element={<TestPage />} />
         <Route path="/flagger" element={<TheFlagger />} />
         <Route path="/speeder" element={<TheSpeeder />} />
-        <Route path="/thesvg" element={<TheSvg />} />
       </Routes>
     </BrowserRouter>
   );
